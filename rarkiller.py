131b1438b1a40d8ff6d74a1bffbf4ebd691dff50
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2012-03-27 18:46:13 -0300

    Initial commit

---
+++ rarkiller.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import re
+
+def main(args):
+    print 'Searching path: %s' % args[1]
+    rar_count = 0
+
+    for dirname, dirnames, filenames in os.walk(args[1]):
+         for filename in filenames:
+             file, extension = os.path.splitext(filename)
+             is_rar = extension == '.rar' or re.search(r'\.r\d+$', extension)
+             if is_rar:
+                 filepath = os.path.join(dirname, filename)
+                 print 'Deleting: %s' % filepath    
+                 os.remove(filepath)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
